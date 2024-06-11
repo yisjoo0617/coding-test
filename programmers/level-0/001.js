16c95aa88b785a1ee6ef08b70a6efbcf888fe322
seonkyung <yisjoo0617@gmail.com>
2024-06-12 02:13:15 +0900

    level-0

--- programmers/level-0/001.js
+++ programmers/level-0/001.js
@@ -1,5 +1,6 @@
 /**
- * 문제 설명
+ * 카운트 다운
+문제 설명
 정수 start_num와 end_num가 주어질 때, start_num에서 end_num까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
@@ -14,7 +15,6 @@ start_num	end_num	    result
 
 10부터 3까지 1씩 감소하는 수를 담은 리스트는 [10, 9, 8, 7, 6, 5, 4, 3]입니다.
  */
-
 
 function solution(start, end_num) {
     var answer = [];
